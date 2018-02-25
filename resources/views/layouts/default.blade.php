37a3748446d72afbef95da7798f1fb02b357be12
Jeremiah Lutz <bullfrog245@gmail.com>
2018-02-25 18:36:03 -0500

    Modest branding update

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,33 +4,45 @@
  */
 ?><!doctype html>
 <html lang="en">
-  <head>
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Comfortaa|Permanent+Marker" rel="stylesheet"> 
+    <link href="/css/default.css" rel="stylesheet">
 
     <title>@yield('page_title')</title>
-  </head>
+</head>
 
-  <body>
-    <div class="container">
-      <h1>White Trash Fabulous</h1>
-    </div>
+<body>
+    <header class="header">
+        <div class="container">
+            <h1 class="site-name">
+                <a href="/" title="White Trash Fabulous Trading">
+                    <span>WTF</span>trading
+                </a>
+            </h1>
+        </div>
+    </header>
 
-    @yield('content')
+    <main class="main">
+        @yield('content')
+    </main>
 
     <footer class="footer">
-      <div class="container">
-          &copy; {{ date('Y') }} Bullfrog Labs, Inc.
-      </div>
+        <div class="container">
+            &copy; {{ date('Y') }} White Trash Fabulous Trading ::
+            <a href="https://www.bullfroglabs.net">Website by Bullfrog Labs, Inc.</a>
+        </div>
     </footer>
+
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-  </body>
+</body>
 </html>
